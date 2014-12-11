166793fae1e0e4ceaf134bdf1d7502d21440a65c
Brancinho <branko.bauk@gmail.com>
2014-12-11 22:49:24 +0100

    commit

---
+++ SellingReport.Models/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SellingReport.Models.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductSellingPlans = new List<ProductSellingPlan>();
+            ProductSellingReports = new List<ProductSellingReport>();
+        }
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        private ICollection<ProductSellingPlan> ProductSellingPlans { get; set; }
+        private ICollection<ProductSellingReport> ProductSellingReports { get; set; }
+    }
+}
