d15db9dac2ffa2c43169fb67c06be423660a47f7
Brancinho <branko.bauk@gmail.com>
2014-12-14 22:47:05 +0100

    commit

---
+++ SellingReport.Models/ViewModels/VariableHolidayViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SellingReport.Models.Models;
+
+namespace SellingReport.Models.ViewModels
+{
+    public class VariableHolidayViewModel
+    {
+        public VariableHoliday VariableHoliday { get; set; }
+        public IEnumerable<SelectListItem> Countries { get; set; }
+    }
+}
