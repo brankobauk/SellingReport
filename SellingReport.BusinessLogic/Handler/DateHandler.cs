569e396943ca5bbab57f6fac05f3ec278b7997d5
Brancinho <branko.bauk@gmail.com>
2014-12-28 13:08:18 +0100

    commit

---
+++ SellingReport.BusinessLogic/Handler/DateHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SellingReport.Models.Models;
+
+namespace SellingReport.BusinessLogic.Handler
+{
+    public class DateHandler
+    {
+        public DateTime GetLastActivityDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
