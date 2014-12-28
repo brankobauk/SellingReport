e319eea372b826e5f6dac0c09369cf0b2aa36946
Brancinho <branko.bauk@gmail.com>
2014-12-28 15:49:08 +0100

    date fix

--- SellingReport/Scripts/Holiday/general.js
+++ SellingReport/Scripts/Holiday/general.js
@@ -2,4 +2,21 @@
     $(".date").datepicker({
          dateFormat: 'dd.mm',
     });
+    jQuery.validator.addMethod(
+            'date',
+            function (value, element, params) {
+                if (this.optional(element)) {
+                    return true;
+                };
+                var result = false;
+                try {
+                    $.datepicker.parseDate('dd.mm', value);
+                    result = true;
+                } catch (err) {
+                    result = false;
+                }
+                return result;
+            },
+            ''
+        );
 });
